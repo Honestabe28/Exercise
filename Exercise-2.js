efedaec2375ee90bc0cd89d19b9a296467c56737
WASHINGTON Abraham <53501@2021-53501.local>
2019-01-06 12:46:02 +0700

    Error

---
+++ Exercise-2.js
@@ -0,0 +1,26 @@
+// var a= document.getElementByxId("Num1").value;
+// var b= document.getElementById("Num2").value;
+
+function multiply(){
+	var a= document.getElementById("Num1").value;
+	var b= document.getElementById("Num2").value;
+
+
+	document.getElementById("result").innerHTML=a*b;
+}
+	
+function divide(){
+	a = document.getElementById("Num1").value;
+	b = document.getElementById("Num2").value;
+
+
+	document.getElementById("result").innerHTML=a*b;
+}
+
+function add(){
+
+}
+
+function substract(){
+
+}
